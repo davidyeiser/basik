b2ccf50f44c4b8e8d241df7d5bc9bb4a457c0ef2
David Yeiser <david@davidyeiser.com>
2018-02-16 10:36:11 -0500

    Moved mobile menu nav button in has_nav_menu() conditional. Fixes #2

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <header class="site-header">
   <a class="identity" href="<?php echo get_home_url(); ?>" itemProp="url" itemScope="itemscope" itemType="https://schema.org/Brand"><?php echo get_bloginfo('name'); ?></a>
 
-  <a id="js-menu-control" class="js-menu-control menu-control" href="#main-nav" onclick="return false;"><span class="hide">Menu</span></a>
-
   <div class="frame">
     <?php if (has_nav_menu('main_navigation')) : ?>
-    <nav id="main-nav" class="main-nav measure">
-      <?php wp_nav_menu(['theme_location' => 'main_navigation', 'menu_class' => 'main-nav-list nav-list']); ?>
-    </nav>
+      <a id="js-menu-control" class="js-menu-control menu-control" href="#main-nav" onclick="return false;"><span class="hide">Menu</span></a>
+
+      <nav id="main-nav" class="main-nav measure">
+        <?php wp_nav_menu(['theme_location' => 'main_navigation', 'menu_class' => 'main-nav-list nav-list']); ?>
+      </nav>
     <?php endif; ?>
   </div>
 </header>
